ef35edb45a25ae921bd7076fdcd33fb5780ef8b5
Maksim Kalnik <kalnikmaxim@gmail.com>
2017-11-10 13:10:32 +0200

    fixes

--- src/js/local.js
+++ src/js/local.js
@@ -4,11 +4,10 @@
 $(document).ready(function () {
     var body = $('body');
 
-    body.on('click', '.js-table', function(event){
+    body.on('click', '.mobile-only-show', function(event){
         event.preventDefault();
         body.append($('.map-container'));
         $('.map-container').addClass('show');
-        body.scrollTop(0);
         $('.page__wrap').hide();
 
         return false
@@ -21,6 +20,26 @@ $(document).ready(function () {
         $('.js-before-table').after($('.map-container'));
         $('.page__wrap').show();
         $('html, body').animate({ scrollTop: $('.map-container').position().top }, 500);
+        return false
+
+    });
+
+    body.on('click', '.map-container__zoom', function(event){
+        event.preventDefault();
+        $('.blur').addClass('show');
+        $('.popup').addClass('show');
+        $('.popup').append($('.map-container'));
+
+        return false
+
+    });
+
+    body.on('click', '.popup__close.btn', function(event){
+        event.preventDefault();
+        $('.blur').removeClass('show');
+        $('.popup').removeClass('show');
+        $('.text-box--map').append($('.map-container'));
+
         return false
 
     });
